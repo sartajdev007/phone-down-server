38df8f4f2e69cdc6fd94f7442898d5241b85d63c
sartajdev007 <sartaj5akik@gmail.com>
2022-11-26 02:09:26 +0600

    make admin api

--- index.js
+++ index.js
@@ -10,6 +10,22 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization
+    if (!authHeader) {
+        return res.status(401).send('unauthorized access')
+    }
+    const token = authHeader.split(' ')[1]
+
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden Access' })
+        }
+        req.decoded = decoded
+        next()
+    })
+}
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ddhvpui.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -35,14 +51,20 @@ async function run() {
             res.send(users)
         })
 
-
-        app.get('/users/allbuyers', async (req, res) => {
-            const query = { role: 'buyer' };
-            const buyers = await usersCollection.find(query).toArray();
-            res.send(buyers)
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.send(result)
         })
 
-        app.put('/users/allbuyers/admin/:id', async (req, res) => {
+        app.put('/users/admin/:id', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
+            const query = { email: decodedEmail }
+            const user = await usersCollection.findOne(query)
+            if (user.status !== 'admin') {
+                return res.status(403).send('Forbidden Access')
+            }
+
             const id = req.params.id;
             const filter = { _id: ObjectId(id) };
             const options = { upsert: true };
@@ -52,19 +74,34 @@ async function run() {
                 }
             }
             const result = await usersCollection.updateOne(filter, updatedUser, options)
+            res.send(result)
         })
 
 
+        // Buyers api
+        app.get('/users/allbuyers', async (req, res) => {
+            const query = { role: 'buyer' };
+            const buyers = await usersCollection.find(query).toArray();
+            res.send(buyers)
+        })
+
+        // app.put('/users/allbuyers/admin/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const filter = { _id: ObjectId(id) };
+        //     const options = { upsert: true };
+        //     const updatedUser = {
+        //         $set: {
+        //             status: 'admin'
+        //         }
+        //     }
+        //     const result = await usersCollection.updateOne(filter, updatedUser, options)
+        // })
+
+        // sellers api
         app.get('/users/allsellers', async (req, res) => {
             const query = { role: 'seller' };
             const sellers = await usersCollection.find(query).toArray();
             res.send(sellers)
-        })
-
-        app.post('/users', async (req, res) => {
-            const user = req.body;
-            const result = await usersCollection.insertOne(user);
-            res.send(result)
         })
 
     }
