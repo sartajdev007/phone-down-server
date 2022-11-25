a71c530a9e96a9d9ebad1230e54b2b7436f9d01c
sartajdev007 <sartaj5akik@gmail.com>
2022-11-26 00:03:01 +0600

    jwt token issue

--- index.js
+++ index.js
@@ -18,6 +18,17 @@ async function run() {
     try {
         const usersCollection = client.db('phoneDown').collection('users')
 
+        app.get('/jwt', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email }
+            const user = await usersCollection.findOne(query)
+            if (user) {
+                const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '2h' })
+                return res.send({ accessToken: token })
+            }
+            res.status(403).send('Forbidden Access')
+        })
+
         app.post('/users', async (req, res) => {
             const user = req.body;
             const result = await usersCollection.insertOne(user);
