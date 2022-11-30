40cdc5b0fc51f4ecb0a7ded36cbf8abf5357622f
sartajdev007 <sartaj5akik@gmail.com>
2022-11-30 16:00:19 +0600

    vercel deploy

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ddhvpui.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
 function verifyJWT(req, res, next) {
     const authHeader = req.headers.authorization
     if (!authHeader) {
@@ -27,10 +32,6 @@ function verifyJWT(req, res, next) {
         next()
     })
 }
-
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ddhvpui.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
     try {
